979063fa0098498f4fc5368868c65176262390b3
Марков Михаил <misha.markov-03@mail.ru>
2021-11-05 13:34:10 +0300

    Add new commands

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,7 +5,9 @@ async def set_default_commands(dp: Dispatcher):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("start", "Запустить бота"),
-            types.BotCommand("help", "Вывести справку"),
             types.BotCommand("convert", "Начать отправку фото"),
+            types.BotCommand("settings", "Настройки"),
+            types.BotCommand("help", "Вывести справку"),
+            types.BotCommand("support", "Техническая поддержка")
         ]
     )
