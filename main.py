2ef17249c8215a87d026aaa01640c7bea045b5e5
Марков Михаил <misha.markov-03@mail.ru>
2021-11-06 08:56:30 +0300

    Creating data folder if not exists

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from utils.set_bot_commands import set_default_commands
 
 async def on_startup(dp: Dispatcher):
     logging.basicConfig(level=logging.INFO)
+    pathlib.Path('data').mkdir(exist_ok=True)
     for id_val in os.listdir(pathlib.Path('data')):
         shutil.rmtree(pathlib.Path('data', id_val))
     await set_default_commands(dp)
